4192962136879e74d5f23c3af52f3636aeb07812
WinnyKay3 <winffredkalondu@gmail.com>
2024-02-08 19:10:22 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Function that filters datum"""
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Filter datum filters fields with redaction"""
     for field in fields:
         message = re.sub(
