2398d90e17c4a76bc989482b39160a8379ae40ae
WinnyKay3 <winffredkalondu@gmail.com>
2024-02-08 18:46:03 +0000

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function that filters datum"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """Filter datum filters fields with redaction"""
+    for field in fields:
+        message = re.sub(
+            fr"({field}=)([^{separator}]+)", fr"\1{redaction}", message)
+    return message
